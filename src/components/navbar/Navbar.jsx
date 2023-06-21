56aaf52e8293bda1a23b96e048cafb30bc9fb388
Hamza Mushtaq <hamza.mushtaq@apimio.com>
2023-06-21 18:51:01 +0500

    initail commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='pacifico text-[40px] absolute left-1/2 transform translate-x-[-50%]'>Navbar</div>
+  )
+}
+
+export default Navbar
