324fc07335d9d9a0b04153009042dae50fe5f848
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2023-08-02 00:45:51 +0500

    add: vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { PageProvider } from '@/context/pageContext'
 import Navbar from '@/components/navbar/Navbar'
 import Head from "next/head";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,13 +17,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-  <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-</Head>
+        <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+      </Head>
       <body className={inter.className}>
         <PageProvider>
           <Navbar />
           {children}
+          <Analytics />
         </PageProvider>
       </body>
     </html>
